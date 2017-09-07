1e3cd8c8853629ccbd30f81cbc7126379ea98bf9
mattias josephsson <josephson199@gmail.com>
2017-09-07 09:48:41 +0200

    - Lägg till ett DbSet av Product med namnet Products till ApplicationDbContext.

--- DiagnosticTestAspNet2Project/Data/ApplicationDbContext.cs
+++ DiagnosticTestAspNet2Project/Data/ApplicationDbContext.cs
@@ -13,6 +13,7 @@ namespace DiagnosticTestAspNet2Project.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+
         }
 
         protected override void OnModelCreating(ModelBuilder builder)
@@ -22,5 +23,7 @@ namespace DiagnosticTestAspNet2Project.Data
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
         }
+
+        public DbSet<Product> Products { get; set; }
     }
 }
